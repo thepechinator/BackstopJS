b4e84e04e11be3b6e8b8ee8438b988903bd488d6
William Ferguson <wffergusonjr@gmail.com>
2016-04-20 21:04:31 -0400

    Remove, cleanup comments

--- compare/src/app/components/screenshotDetails/screenshotDetails.directive.js
+++ compare/src/app/components/screenshotDetails/screenshotDetails.directive.js
@@ -4,9 +4,6 @@ export function ScreenshotDetailsDirective() {
   let directive = {
     restrict: 'EA',
     templateUrl: 'app/components/screenshotDetails/screenshotDetails.html',
-/*    scope: {
-        creationDate: '='
-    },*/
     controller: screenshotDetailsController,
     controllerAs: 'vm',
     bindToController: true
@@ -18,8 +15,5 @@ export function ScreenshotDetailsDirective() {
 class screenshotDetailsController {
   constructor () {
     'ngInject';
-
-    // "this.creationDate" is available by directive option "bindToController: true"
-    //this.relativeDate = moment(this.creationDate).fromNow();
   }
 }
